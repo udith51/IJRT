cd5db174b4d4adc1b9a1c37db6ce37954d40f4b2
devika <devikavarshney0003@gmail.com>
2021-11-30 03:04:59 +0530

    changed login button and added script.js

---
+++ Src/script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("child11");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
