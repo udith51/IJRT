58bf9dfe3caba502419eb328f751fb846486325a
Udith Shyamsukha <udithshyamsukha@gmail.com>
2021-12-03 17:50:52 +0530

    Connected to MySQL DB

--- app.js
+++ app.js
@@ -18,6 +18,26 @@ const handlebars = exphbs.create({ extname: "hbs" });
 app.engine('hbs', handlebars.engine);
 app.set('view engine', 'hbs');
 
+//Connection Pool
+const pool = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+//Connect to DB
+pool.getConnection((err, connection) => {
+    if (err)
+        throw err;
+    console.log('Connected to ID: ' + connection.threadId);
+})
+
+
+
+
+
 //Router
 app.get('', (req, res) => {//home page
     res.render('home');
