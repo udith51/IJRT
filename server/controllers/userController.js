cc0741b0b5e77ad748ff02e8125790e8d84a7d4b
Udith Shyamsukha <udithshyamsukha@gmail.com>
2021-12-04 03:02:52 +0530

    Added search functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -11,15 +11,32 @@ const pool = mysql.createPool({
 
 //View
 exports.view = (req, res) => {
-    // res.render('home');
-
-
     pool.getConnection((err, connection) => {
         if (err)
             throw err;
         console.log('Connected to ID: ' + connection.threadId);
 
         connection.query('SELECT * FROM USER', (err, rows) => {
+            connection.release();
+            if (!err) {
+                res.render('home', { rows });
+                console.log('Here');
+            } else {
+                console.log(err);
+            }
+            console.log(rows);
+        });
+    });
+}
+
+exports.find = (req, res) => {
+    pool.getConnection((err, connection) => {
+        if (err)
+            throw err;
+        console.log('Connected to ID: ' + connection.threadId);
+
+        let searchBody = req.body.search;
+        connection.query('SELECT * FROM USER WHERE first_name LIKE ? OR last_name LIKE ?', ['%' + searchBody + '%', '%' + searchBody + '%'], (err, rows) => {
             connection.release();
             if (!err) {
                 res.render('home', { rows });
--- server/routes/user.js
+++ server/routes/user.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/userController')
 
 
 router.get('/', userController.view);
+router.post('/', userController.find);
 
 module.exports = router;
